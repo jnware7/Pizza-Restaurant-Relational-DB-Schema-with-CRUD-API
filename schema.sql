0859fa862e765cd7c7f318a54d0eee84fe062b79
jnware7 <jnware7@gmail.com>
2017-06-27 14:00:37 -0700

    rebuilt schema and er diagram

--- schema.sql
+++ schema.sql
@@ -1,76 +1,110 @@
-
-CREATE TABLE customer(
-  id SERIAL,
-  name VARCHAR,
-  address VARCHAR,
-  phone_num VARCHAR,
-  user_name VARCHAR,
-  password VARCHAR,
-  pay_method VARCHAR,
-  prefrence_id INTEGER,
-  pizzadata_id INTEGER,
-  drink_id INTEGER
+CREATE TABLE customer (
+customer_id  SERIAL ,
+name VARCHAR ,
+address VARCHAR ,
+phone_num INTEGER ,
+payment_meth TEXT ,
+PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE pizzadata(
-  id SERIAL,
-  size VARCHAR,
-  type VARCHAR,
-  pizza_id INTEGER,
-  price VARCHAR,
+CREATE TABLE card_join (
+customer_id  SERIAL ,
+card_id INTEGER ,
+PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE prefrence(
-  id SERIAL,
-  customer_id INTEGER,
-  pizzadata_id VARCHAR
+CREATE TABLE orders_join (
+customer_id  SERIAL ,
+orders_id INTEGER ,
+PRIMARY KEY (customer_id)
 );
 
-CREATE TABLE ingredients(
-  id SERIAL,
-  name VARCHAR
+CREATE TABLE card (
+id  SERIAL ,
+card_num INTEGER ,
+PRIMARY KEY (id)
 );
 
-CREATE TABLE drinks(
-  id SERIAL,
-  description VARCHAR,
-  manufacturer VARCHAR,
-  supplier VARCHAR,
-  price VARCHAR
+CREATE TABLE orders(
+id  SERIAL ,
+orders_date INTEGER ,
+PRIMARY KEY (id)
 );
 
-CREATE TABLE pizza(
-  id SERIAL,
-  ingredients_id INTEGER,
-  name VARCHAR,
-  add_on VARCHAR DEFAULT NULL
+CREATE TABLE pizza_data (
+pizza_data_id  SERIAL ,
+size VARCHAR ,
+type VARCHAR ,
+price DECIMAL ,
+happy_hour BOOLEAN NOT NULL DEFAULT 'False' ,
+PRIMARY KEY (pizza_data_id)
 );
 
-INSERT INTO ingredients(name)
-VALUES('Peppers');
-INSERT INTO ingredients(name)
-VALUES('Onions');
-INSERT INTO ingredients(name)
-VALUES('Olives');
-INSERT INTO ingredients(name)
-VALUES('Mushrooms');
-INSERT INTO ingredients(name)
-VALUES('Cheese');
-INSERT INTO ingredients(name)
-VALUES('Pepperoni');
+CREATE TABLE pizza_data_join (
+pizza_data_id  SERIAL ,
+orders_id INTEGER ,
+PRIMARY KEY (pizza_data_id)
+);
 
+CREATE TABLE ingredients (
+id  SERIAL ,
+name VARCHAR ,
+price DECIMAL ,
+PRIMARY KEY (id)
+);
 
-INSERT INTO pizza(ingredients_id, name)
-VALUES(5,'Cheese');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(6,'Pepperoni');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(5,'Pepperoni');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(1,'Veggie');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(2,'Veggie');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(3,'Veggie');
-INSERT INTO pizza(ingredients_id, name)
-VALUES(4,'Veggie');
+CREATE TABLE ingredients_join (
+pizza_data_id  SERIAL ,
+ingredients_id INTEGER ,
+PRIMARY KEY (pizza_data_id)
+);
+
+CREATE TABLE drinks (
+id  SERIAL ,
+description VARCHAR ,
+manufacturer VARCHAR ,
+supplier VARCHAR ,
+price DECIMAL ,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE drinks_join (
+orders_id  SERIAL ,
+drink_id INTEGER ,
+PRIMARY KEY (orders_id)
+);
+
+CREATE TABLE preference (
+id  SERIAL ,
+pizza_data_id INTEGER ,
+PRIMARY KEY (id)
+);
+
+CREATE TABLE preference_join (
+preference_id  SERIAL ,
+customer_id INTEGER ,
+PRIMARY KEY (preference_id)
+);
+
+CREATE TABLE user_login (
+customer_id  SERIAL ,
+user_login_name VARCHAR ,
+password VARCHAR ,
+salt VARCHAR ,
+PRIMARY KEY (customer_id)
+);
+
+ALTER TABLE card_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE card_join ADD FOREIGN KEY (card_id) REFERENCES card (id);
+ALTER TABLE orders_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE orders_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE pizza_data ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data_join (pizza_data_id);
+ALTER TABLE pizza_data ADD FOREIGN KEY (pizza_data_id) REFERENCES ingredients_join (pizza_data_id);
+ALTER TABLE pizza_data_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE ingredients_join ADD FOREIGN KEY (ingredients_id) REFERENCES ingredients (id);
+ALTER TABLE drinks_join ADD FOREIGN KEY (orders_id) REFERENCES orders (id);
+ALTER TABLE drinks_join ADD FOREIGN KEY (drink_id) REFERENCES drinks (id);
+ALTER TABLE preference ADD FOREIGN KEY (pizza_data_id) REFERENCES pizza_data (pizza_data_id);
+ALTER TABLE preference_join ADD FOREIGN KEY (preference_id) REFERENCES preference (id);
+ALTER TABLE preference_join ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
+ALTER TABLE user_login ADD FOREIGN KEY (customer_id) REFERENCES customer (customer_id);
